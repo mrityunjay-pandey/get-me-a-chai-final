8e34a3f5995c69733bb6630f46a8197e0d89bbfa
Mrityunjay <mrityunjayp1409@gmail.com>
2025-04-06 14:38:08 +0530

    1:15:29 if the user is logged in then he is getting redirected to /dashboard page using useRouter and router.push from next/navigation

--- Desktop/get-me-a-chai/get-me-a-chai/app/login/page.js
+++ Desktop/get-me-a-chai/get-me-a-chai/app/login/page.js
@@ -1,8 +1,15 @@
 "use client"
 import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/navigation'
 
 const Login = () => {
+    const {data: session} = useSession()
+
+    if(session) {
+      const router = useRouter()
+      router.push('/dashboard')
+    }
   return (
     <div className='text-white py-14 container mx-auto'>
         <h1 className='font-bold text-3xl text-center'>

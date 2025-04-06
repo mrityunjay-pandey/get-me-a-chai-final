6f4b2087806926323049e98162344ca096f43562
Mrityunjay <mrityunjayp1409@gmail.com>
2025-04-06 13:23:57 +0530

    authentication done using github next step is to redirect users when they get logged in

--- Desktop/get-me-a-chai/get-me-a-chai/app/layout.js
+++ Desktop/get-me-a-chai/get-me-a-chai/app/layout.js
@@ -1,9 +1,10 @@
-import {Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Get me a chai - Fund your projects with chai",
@@ -14,11 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <SessionWrapper>
+        <Navbar />
         <div className="min-h-[87vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-        {children}
+          {children}
         </div>
-        <Footer/>
+        <Footer />
+        </SessionWrapper>
       </body>
     </html>
   );
--- Desktop/get-me-a-chai/get-me-a-chai/components/Navbar.js
+++ Desktop/get-me-a-chai/get-me-a-chai/components/Navbar.js
@@ -1,7 +1,16 @@
+"use client"
 import React from "react";
+import {useSession, signIn, signOut} from "next-auth/react"
 import Link from 'next/link';
 
 const Navbar = () => {
+    const { data: session } = useSession()
+    if(session) {
+        return <>
+          Signed in as {session.user.email} <br/>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      }
   return (
     <nav className="bg-gray-900 text-white flex justify-between items-center px-4 h-16">
       <div className="logo font-bold text-lg flex justify-center items-center">
@@ -17,7 +26,7 @@ const Navbar = () => {
         </ul> */}
       <div>
         <Link href={"/login"}>
-        <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+        <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 " >
           Login
         </button></Link>
       </div>
